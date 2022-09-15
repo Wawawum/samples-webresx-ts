b950215b88bdf399e9b1fb0ffa2603ce5d472a7a
Benjamin Bertin <benjamin@wawawum.com>
2022-09-15 13:25:47 -0400

    lodash

--- typescript-thirdparty/src/contact_form.ts
+++ typescript-thirdparty/src/contact_form.ts
@@ -1,5 +1,10 @@
+import * as _ from 'lodash';
+
 export const onContactFormLoad = function(executionContext: Xrm.Events.EventContext) {
 
+    const technologies = ['Canvas App', 'Dataverse', 'Model-Driven App', 'Power Automate', 'Power BI'];
+    const suffledTech = _.shuffle(technologies).join(', ');
+
     const formContext = executionContext.getFormContext();
-    formContext.ui.setFormNotification('Hello world (part 2)!', 'INFO', 'www_contactform_notification');
+    formContext.ui.setFormNotification(`Here are some things you may interest you in no particular order: ${suffledTech}.`, 'INFO', 'www_contactform_notification');
 };

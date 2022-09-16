75d116fe951f1c47dff812b622b3065255683702
Benjamin Bertin <benjamin@wawawum.com>
2022-09-16 07:20:15 -0400

    Big Bang HTML webresource

---
+++ typescript-htmlwebresource/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    www_crm: './src/crm.ts',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      name: 'www',
+      type: 'assign-properties',
+    }
+  },
+};
